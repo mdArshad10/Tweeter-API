b68f67d13d9a9d5ce7f765bb458dd0f6cd350ead
Md Arshad <mdarshd246@gmail.com>
2024-08-04 12:43:23 +0530

    added the tweet Respository

--- src/repository/tweet.repository.js
+++ src/repository/tweet.repository.js
@@ -1,8 +1,57 @@
 const { Tweet } = require('../models/index.js');
 
 class TweetRepository {
-	async create() {}
-	async delete() {}
+	async create(data) {
+		try {
+			const response = await Tweet.create(data);
+			return response;
+		} catch (error) {
+			console.log('some error on repository level');
+			throw error;
+		}
+	}
+
+	async get(id) {
+		try {
+			const response = await Tweet.findById(id);
+			return response;
+		} catch (error) {
+			console.log('some error on repository level');
+			throw error;
+		}
+	}
+
+	async getWithComment(id) {
+		try {
+			const response = await Tweet.findById(id);
+			return response;
+		} catch (error) {
+			console.log('some error on repository level');
+			throw error;
+		}
+	}
+
+	async destroy(id) {
+		try {
+			const response = await Tweet.findByIdAndDelete(id);
+			return response;
+		} catch (error) {
+			console.log('some error on repository level');
+			throw error;
+		}
+	}
+
+	async getAll(offset, limit) {
+		try {
+			const response = await Tweet.find()
+				.skip(offset)
+				.limit(limit);
+			return response;
+		} catch (error) {
+			console.log('some error on repository level');
+			throw error;
+		}
+	}
 }
 
 module.exports = TweetRepository;
